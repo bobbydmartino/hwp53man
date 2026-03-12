7b6bd9753817958d976cac72b84124a817f3f9a6
Robert Martino <bobbyd@amazo.local>
2026-03-11 20:40:53 -0400

    propopperreference

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addGlobalData("env", process.env);
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("reference");
 
 
   const pathPrefix = "/";
